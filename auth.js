abd91bbcdb9986c3fdfa3f1c96b7934d9d9be7ab
ArielGoulart <31768080+ArielGoulart@users.noreply.github.com>
2019-12-10 18:31:54 -0300

    Firebase Auth - Users Sign Up, Login and Logout

--- auth.js
+++ auth.js
@@ -1,4 +1,52 @@
+// listen user status
+auth.onAuthStateChanged(user=>{
+
+  if(user) //user != null 
+  {
+    console.log('user logged in: ', user)
+  }
+  else{
+    console.log('user logged out: ', user)
+  }
+
+});
+
+// sign up
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e)=>{
   e.preventDefault();
-})
+
+  // get info user
+  const email = signupForm['signup-email'].value;
+  const password = signupForm['signup-password'].value;
+
+  auth.createUserWithEmailAndPassword(email, password).then(cred=>{
+    const modal = document.querySelector('#modal-signup');
+    M.Modal.getInstance(modal).close();
+    signupForm.reset();
+  })
+});
+
+//logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e)=>{
+  e.preventDefault();
+  auth.signOut().then(()=>{
+  })
+});
+
+//login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e)=>{
+  e.preventDefault();
+
+  //get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  auth.signInWithEmailAndPassword(email, password).then(cred=>{
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  })
+});
