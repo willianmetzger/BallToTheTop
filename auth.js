a27d98b84ba2e72dfd4798fd8209007366f900bd
ArielGoulart <31768080+ArielGoulart@users.noreply.github.com>
2019-12-10 16:23:35 -0300

    comeitei fds

--- auth.js
+++ auth.js
@@ -1,37 +1,4 @@
-var provider = new firebase.auth.GoogleAuthProvider();
-
-firebase.auth().signInWithPopup(provider).then(function(result) {
-  // This gives you a Google Access Token. You can use it to access the Google API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
-
-firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // ...
-  }
-  // The signed-in user info.
-  var user = result.user;
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e)=>{
+  e.preventDefault();
+})
--- init-firebase.js
+++ init-firebase.js
@@ -1,7 +1,20 @@
 // TODO: Replace the following with your app's Firebase project configuration
 var firebaseConfig = {
-    // ...
+    apiKey: "AIzaSyC1nwfECzAMJSfviqqPabKExmergNWr_hM",
+    authDomain: "balltothetop-abfe0.firebaseapp.com",
+    databaseURL: "https://balltothetop-abfe0.firebaseio.com",
+    projectId: "balltothetop-abfe0",
+    storageBucket: "balltothetop-abfe0.appspot.com",
+    messagingSenderId: "108301361663",
+    appId: "1:108301361663:web:aedee872f4d6541516c7ca",
+    measurementId: "G-TEHYNVVM11"
   };
   
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  // Initialize Firebase with a "default" Firebase project
+var defaultProject = firebase.initializeApp(firebaseConfig);
+
+console.log(defaultProject.name);  // "[DEFAULT]"
+
+// Option 1: Access Firebase services via the defaultProject variable
+var defaultStorage = defaultProject.storage();
+var defaultFirestore = defaultProject.firestore();
